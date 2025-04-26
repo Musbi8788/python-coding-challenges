4cbc7d969e6b472a6e6854502f0f34c3c73faed6
Musbi <musbi700@gmail.com>
2025-04-26 00:42:36 -0700

    Update main.py

--- challenges/1.2.Escape_Characters/main.py
+++ challenges/1.2.Escape_Characters/main.py
@@ -1,4 +1,4 @@
 string_escape = 'Hello,\n this is escape string' # change this line
 
 # change the string_escape, the string must contain "new line"
-print(string_escape)
+print(f'\n{string_escape}')
