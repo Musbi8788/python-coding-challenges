449ee981346f8e4c853ae358ef1cb684af84edcf
Musbi <musbi700@gmail.com>
2025-04-26 00:44:30 -0700

    Update main.py

--- challenges/2.2.Strings/main.py
+++ challenges/2.2.Strings/main.py
@@ -6,6 +6,6 @@ myAge = "20"
 
 favoriteActivity = "Programming"
 
-mySentence = "The world is changing with the programmers" 
+mySentence = "The world is changing by the programmers" 
 
 ### Modify the code above ###
