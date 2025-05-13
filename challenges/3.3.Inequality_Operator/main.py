03c1dd87f10c66a9cf1db7508d7d878fd7f8c011
Musbi <musbi700@gmail.com>
2025-05-13 16:49:14 -0700

    Update main.py

--- challenges/3.3.Inequality_Operator/main.py
+++ challenges/3.3.Inequality_Operator/main.py
@@ -1,6 +1,6 @@
 def inequality(value):
     # Complete the if statement on the next line using value, the inequality operator (!=), and the number 13.
-    if value != 13:
+    if value not == 13:
     ### Your code goes above this line ###
         return "Not Equal to 13"
     else:
