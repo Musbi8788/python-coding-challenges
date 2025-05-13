87dbd6eb475d07f1d86d45a9990db4342f3b786c
Musbi <musbi700@gmail.com>
2025-05-13 16:47:08 -0700

    Update main.py

--- challenges/3.2.Equality_Operator/main.py
+++ challenges/3.2.Equality_Operator/main.py
@@ -1,6 +1,6 @@
 def equality(value):
     # Complete the if statement on the next line using value, the equality operator (==), and the number 12.
-    if :
+    if  value == 12:
     ### Your code goes above this line ###
         return "Equal to 12"
     else:
