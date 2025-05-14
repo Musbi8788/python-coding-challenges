f6a8490b0a7167ca3cf951c03a800123f95a671a
Musbi <musbi700@gmail.com>
2025-05-14 15:43:53 -0700

    Update main.py

--- challenges/3.7.Strictly_Greater_Than_Operator/main.py
+++ challenges/3.7.Strictly_Greater_Than_Operator/main.py
@@ -1,10 +1,10 @@
 def strictly_greater_than(value):
-    if value :  # Change this line
+    if value > 100:  # Change this line
         return "Greater than 100"
-    elif value :  # Change this line
+    elif value > 10:  # Change this line
         return "Greater than 10"
     else:
         return "10 or less"
 
 # Change the value 1 below to experiment with different values
-print(strictly_greater_than(1))
+print(strictly_greater_than(89))
