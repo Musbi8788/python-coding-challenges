1c010601565ba82403023a04fde530fdfe0ff9cb
Musbi <musbi700@gmail.com>
2025-05-14 15:39:36 -0700

    Update main.py

--- challenges/3.4.Strictly_Less_Than_Operator/main.py
+++ challenges/3.4.Strictly_Less_Than_Operator/main.py
@@ -1,10 +1,10 @@
 def strictly_less_than(value):
-    if value < 10:  # Change this line
+    if value < 10:  
         return "Less than 10"
-    elif value < 100 :  # Change this line
+    elif value < 100 : 
         return "Less than 100"
     else:
         return "100 or more"
 
 # Change the value 1 below to experiment with different values
-print(strictly_less_than(1))
+print(strictly_less_than(4))
