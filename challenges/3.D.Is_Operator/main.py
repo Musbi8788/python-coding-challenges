d45d8c36f7c8c764c592c36b4b148177efdbd2d6
Musbi <musbi700@gmail.com>
2025-05-16 13:18:02 -0700

    Update main.py

--- challenges/3.D.Is_Operator/main.py
+++ challenges/3.D.Is_Operator/main.py
@@ -1,8 +1,8 @@
 def identity_test(value):
-    if value :  # Change this line
+    if value is "musbi":  # Change this line
         return "Same object"
     else:
         return "Different object"
 
 # Change the value 42 below to experiment with different values
-print(identity_test(42))
+print(identity_test("musbi"))
