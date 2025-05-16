3c0d8775d6125dfed764e6bdc51ff67b13c54218
Musbi <musbi700@gmail.com>
2025-05-16 13:22:02 -0700

    Update main.py

--- challenges/3.F.In_Operator/main.py
+++ challenges/3.F.In_Operator/main.py
@@ -2,6 +2,6 @@ word = "wrong answer"
 sentence = "The quick brown fox jumped over the lazy dog."
 
 def membership_test():
-    return word in sentence
+    return "quick" in sentence
 
 print(membership_test())
