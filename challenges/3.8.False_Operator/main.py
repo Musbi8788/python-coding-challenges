34b9e798a0931f2687f36506ef331326771c6429
Musbi <musbi700@gmail.com>
2025-05-16 13:07:03 -0700

    Update main.py

--- challenges/3.8.False_Operator/main.py
+++ challenges/3.8.False_Operator/main.py
@@ -1,4 +1,4 @@
 def boolean_false():
-    return value  # Change the variable named value to the correct answer
+    return False  # Change the variable named value to the correct answer
 
 print(boolean_false())
