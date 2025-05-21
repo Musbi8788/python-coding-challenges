8a1bb058def884afc93c838eb6680ebe0f978941
Musbi <187058040+Musbi8788@users.noreply.github.com>
2025-05-21 14:04:43 -0700

    Update main.py

--- challenges/7.1.Loops/main.py
+++ challenges/7.1.Loops/main.py
@@ -1,4 +1,4 @@
-count = 10  # Change this count
+count = 50  # Change this count
 switch_loop = False
 switch_end = False
 
@@ -7,6 +7,7 @@ while count < 9:
   	count = count + 1
   	switch_loop = True
 # use else statements 
+else:
 	print('loop end')
 	switch_end = True;
 
