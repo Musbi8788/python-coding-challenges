6075b510a6a9b649f0092d52ccdd8b4ac6e28654
Musbi <musbi700@gmail.com>
2025-05-22 13:09:28 -0700

    Update main.py

--- challenges/8.3.Function_Documentation_Strings/main.py
+++ challenges/8.3.Function_Documentation_Strings/main.py
@@ -1,6 +1,6 @@
 def docstring_function():
     ### Write your docstring below this line. ###
-
+    """This function is not doing nothing order than passing"""
 
 
     ### Write your docstring above this line. ###
