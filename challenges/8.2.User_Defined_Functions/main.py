c743e0132d6a640a5ca516d0a1d267442030fc73
Musbi <musbi700@gmail.com>
2025-05-22 13:06:04 -0700

    Update main.py

--- challenges/8.2.User_Defined_Functions/main.py
+++ challenges/8.2.User_Defined_Functions/main.py
@@ -1,6 +1,7 @@
 ### Define the say_hello() function and fill in the function body below this line: ###
 
-
+def say_hello():
+  print("Hello World")
 
 ### Call the say_hello() function below this line: ###
 say_hello()
